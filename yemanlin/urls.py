c19e510ec23280a6611efb4c7d8c49d26dae9e7a
fluxlien-droid <fluxlien@gmail.com>
2026-08-19 21:00:45 +0100

    Update urls.py

--- yemanlin/urls.py
+++ yemanlin/urls.py
@@ -6,5 +6,16 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include("store.urls")),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+# MEDIA
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
+
+# STATIC
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
